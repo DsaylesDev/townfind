c9aba79a248cd49e4db64f5dd7daa202cb4255c0
Darion <darionsayles06@gmail.com>
2025-10-30 17:04:10 -0400

    chore: scaffold Spring Boot app

---
+++ backend/src/main/java/com/townfind/TownfindApiApplication.java
@@ -0,0 +1,13 @@
+package com.townfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownfindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownfindApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/townfind/TownfindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.townfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownfindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
